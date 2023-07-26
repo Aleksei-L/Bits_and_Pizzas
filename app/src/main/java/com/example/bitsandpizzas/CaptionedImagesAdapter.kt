942b7f939eb3a788a58c37794199c45e19a6a77e
Aleksei Lobanov <lobanov782@gmail.com>
2023-07-26 19:28:38 +0300

    Added Adapter and RecycleView in PizzaFragment

---
+++ app/src/main/java/com/example/bitsandpizzas/CaptionedImagesAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.bitsandpizzas
+
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class CaptionedImagesAdapter(private val captions: Array<String?>, private val imageIds: Array<Int?>) :
+	RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder>() {
+	class ViewHolder(val cardView: CardView) : RecyclerView.ViewHolder(cardView)
+
+	override fun getItemCount(): Int = captions.size
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+		val cv: CardView = LayoutInflater.from(parent.context).inflate(R.layout.card_captioned_image, parent, false) as CardView
+		return ViewHolder(cv)
+	}
+
+	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+		val cardView: CardView = holder.cardView
+		val imageView: ImageView = cardView.findViewById(R.id.info_image)
+		val textView: TextView = cardView.findViewById(R.id.info_text)
+		val drawable: Drawable? = ContextCompat.getDrawable(cardView.context, imageIds[position]!!)
+
+		imageView.setImageDrawable(drawable)
+		imageView.contentDescription = captions[position]
+		textView.text = captions[position]
+	}
+}
--- app/src/main/java/com/example/bitsandpizzas/PizzaFragment.kt
+++ app/src/main/java/com/example/bitsandpizzas/PizzaFragment.kt
@@ -4,20 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import androidx.fragment.app.ListFragment
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bitsandpizzas.data.Pizza
 
-class PizzaFragment : ListFragment() {
+class PizzaFragment : Fragment() {
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
-	): View? {
-		val adapter = ArrayAdapter(
-			inflater.context,
-			android.R.layout.simple_list_item_1,
-			resources.getStringArray(R.array.pizzas)
-		)
-		listAdapter = adapter
-		return super.onCreateView(inflater, container, savedInstanceState)
+	): View {
+		val pizzaRecycler: RecyclerView = inflater.inflate(R.layout.fragment_pizza, container, false) as RecyclerView
+
+		val pizzaNames: Array<String?> = arrayOfNulls(Pizza.pizzas.size)
+		for (i in pizzaNames.indices)
+			pizzaNames[i] = Pizza.pizzas[i].getName()
+
+		val pizzaImages: Array<Int?> = arrayOfNulls(Pizza.pizzas.size)
+		for (i in pizzaImages.indices)
+			pizzaImages[i] = Pizza.pizzas[i].getImageResourceId()
+
+		val adapter = CaptionedImagesAdapter(pizzaNames, pizzaImages)
+		pizzaRecycler.adapter = adapter
+
+		val layoutManager = GridLayoutManager(activity, 2)
+		pizzaRecycler.layoutManager = layoutManager
+
+		return pizzaRecycler
 	}
 }
